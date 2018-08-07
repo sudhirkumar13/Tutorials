1280713b0225b79e5505fba29e2590090da68fa7
sudhirk13 <sudhir.kumar.m13@gmail.com>
2018-08-07 21:37:08 +0530

    some changes

--- Test_Project/src/com/sbi/mab/Test.java
+++ Test_Project/src/com/sbi/mab/Test.java
@@ -11,11 +11,6 @@ import java.util.List;
 public class Test {
 	public static void main(String[] args) {
 		List<String> lst = Arrays.asList("Sudhir", "Kiah", "Hardik", "Sunil");
-		/*
-		 * lst.forEach(s -> { if ("Kiah".equals(s)) { System.out.println(s); }
-		 * });
-		 */
-
 		lst.stream().filter(s -> s.contains("Kiah")).forEach(System.out::println);
 		System.out.println("Hello Hardik...");
 		System.out.println("Hello Sudhir...");
